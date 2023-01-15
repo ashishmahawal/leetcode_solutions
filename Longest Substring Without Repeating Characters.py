f1755d62640b2ab4d608df3c74dc506192a2ef51
ashishmahawal <amahawal@cs.iitr.ac.in>
2023-01-15 20:18:25 +0530

    p

--- Longest Substring Without Repeating Characters.py
+++ Longest Substring Without Repeating Characters.py
@@ -1,20 +1,14 @@
 def lengthOfLongestSubstring(s: str) -> int:
-    max_win = 0
-    win_list = []
-    size = 0
-    for char in s:
-        if size > max_win:
-                max_win = size
-        if char in win_list:
-            while char in win_list:
-                win_list.pop(0)
-                size-=1 
-            win_list.append(char)
-            size += 1
-        else:
-            win_list.append(char)
-            size+=1
-    return max_win if max_win > size else size
+    l = 0
+    charSet = set()
+    max_size = 0
+    for r in range(len(s)):
+        while s[r] in charSet:
+            charSet.remove(s[l])
+            l+=1
+        charSet.add(s[r])
+        max_size = max(max_size,r - l + 1)
+    return max_size
 
 res = lengthOfLongestSubstring("dvdf")
 print(res)
